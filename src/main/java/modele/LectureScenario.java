c586d744604f6bd87a044a7e710e9bc66d0e20b7
alexi <alexandre.meira06@gmail.com>
2025-06-03 20:05:28 +0200

    ajout de la documentation

--- src/main/java/modele/LectureScenario.java
+++ src/main/java/modele/LectureScenario.java
@@ -3,20 +3,36 @@ package modele;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
-import java.util.List;
 
+/**
+ * Classe LectureScenario permettant de :
+ *     Lire un scénario de transactions entre vendeurs et acheteurs
+ *     Associer les membres à leurs villes
+ *     Regrouper les membres par ville<
+ *     Construire un graphe de livraison avec suffixes + et -
+ *     Représenter ce graphe sous forme de tableau d’adjacence
+ *
+ */
 public class LectureScenario {
+
+    /**
+     * Lit un fichier scénario et extrait deux listes :
+     * une pour les vendeurs et une pour les acheteurs.
+     *
+     * @param scenario Le nom du fichier scénario (ex : "scenario_0.txt")
+     * @return Un tableau contenant [listeVendeur, listeAcheteur]
+     * @throws IDException si une erreur personnalisée est levée
+     * @throws FileNotFoundException si le fichier est introuvable
+     */
     public static List[] lectureScenario(String scenario) throws IDException, FileNotFoundException {
         File file = new File("Scenario" + File.separator + scenario);
-
-//        Scenario scenario = new Scenario();
-
         Scanner scannerFile = new Scanner(file);
         Scanner scannerLine;
+
         ArrayList<String> listeVendeur = new ArrayList<>();
         ArrayList<String> listeAcheteur = new ArrayList<>();
 
-        while(scannerFile.hasNextLine()){
+        while (scannerFile.hasNextLine()) {
             String line = scannerFile.nextLine();
             scannerLine = new Scanner(line).useDelimiter(" ");
             String vendeur = scannerLine.next();
@@ -24,35 +40,21 @@ public class LectureScenario {
             scannerLine.next(); // "->"
             String acheteur = scannerLine.next();
             listeAcheteur.add(acheteur);
-            System.out.println(vendeur + "->" + acheteur);
             scannerLine.close();
         }
         scannerFile.close();
         return new List[]{listeVendeur, listeAcheteur};
     }
 
-//    public static TreeMap<String, String> lectureVilleMembre() throws IDException, FileNotFoundException {
-//        File file = new File("Membre" + File.separator + "membres_APPLI.txt");
-//
-//        Scanner scannerFile = new Scanner(file);
-//        Scanner scannerLine;
-//        TreeMap<String, String> membreVille = new TreeMap<>();
-//
-//        while(scannerFile.hasNextLine()){
-//            String line = scannerFile.nextLine();
-//            scannerLine = new Scanner(line).useDelimiter(" ");
-//            String membre = scannerLine.next();
-//            String ville = scannerLine.next();
-//            membreVille.put(membre, ville);
-//            //System.out.println(membre + "->" + ville);
-//            scannerLine.close();
-//        }
-//        scannerFile.close();
-//        System.out.println(membreVille);
-//        return membreVille;
-//    }
-
-    public static Map lectureVilleMembreCible(ArrayList<String> liste) throws IDException, FileNotFoundException {
+    /**
+     * Extrait les villes associées à une liste de membres à partir du fichier membres_APPLI.txt.
+     *
+     * @param liste Liste d'identifiants de membres
+     * @return Une Map (membre -> ville) uniquement pour les membres spécifiés
+     * @throws IDException si une erreur personnalisée est levée
+     * @throws FileNotFoundException si le fichier est introuvable
+     */
+    public static Map<String, String> lectureVilleMembreCible(ArrayList<String> liste) throws IDException, FileNotFoundException {
         File file = new File("Membre" + File.separator + "membres_APPLI.txt");
 
         Scanner scannerFile = new Scanner(file);
@@ -60,57 +62,67 @@ public class LectureScenario {
         Map<String, String> membreVille = new HashMap<>();
         Map<String, String> membreVilleCible = new HashMap<>();
 
-        while(scannerFile.hasNextLine()){
+        while (scannerFile.hasNextLine()) {
             String line = scannerFile.nextLine();
             scannerLine = new Scanner(line).useDelimiter(" ");
             String membre = scannerLine.next();
             String ville = scannerLine.next();
             membreVille.put(membre, ville);
-            //System.out.println(membre + "->" + ville);
             scannerLine.close();
         }
 
-        for (int i = 0; i < liste.size(); i++) {
-            if (membreVille.containsKey(liste.get(i))) {
-                membreVilleCible.put(liste.get(i), membreVille.get(liste.get(i)));
+        for (String s : liste) {
+            if (membreVille.containsKey(s)) {
+                membreVilleCible.put(s, membreVille.get(s));
             }
         }
 
         scannerFile.close();
-        System.out.println(membreVilleCible);
         return membreVilleCible;
     }
 
-    public static Map regroupementParVille(ArrayList<String> membreVille) throws IDException, FileNotFoundException {
-        Map regroupementVille = lectureVilleMembreCible(membreVille);
-        Map<String, ArrayList> regroupementVilleCible = new HashMap<>();
+    /**
+     * Regroupe une liste de membres par leur ville.
+     *
+     * @param membreVille Liste d’identifiants de membres
+     * @return Map (ville -> liste de membres)
+     * @throws IDException si une erreur personnalisée est levée
+     * @throws FileNotFoundException si le fichier est introuvable
+     */
+    public static Map<String, ArrayList<String>> regroupementParVille(ArrayList<String> membreVille) throws IDException, FileNotFoundException {
+        Map<String, String> regroupementVille = lectureVilleMembreCible(membreVille);
+        Map<String, ArrayList<String>> regroupementVilleCible = new HashMap<>();
 
-        for (int i = 0; i < membreVille.size(); i++) {
-            String ville = regroupementVille.get(membreVille.get(i)).toString();
-            if (!regroupementVilleCible.containsKey(ville)) {  
-                regroupementVilleCible.put(ville, new ArrayList<>());
-            }
-            regroupementVilleCible.get(regroupementVille.get(membreVille.get(i))).add(membreVille.get(i));
+        for (String membre : membreVille) {
+            String ville = regroupementVille.get(membre);
+            regroupementVilleCible.computeIfAbsent(ville, k -> new ArrayList<>()).add(membre);
         }
         return regroupementVilleCible;
     }
 
+    /**
+     * Regroupe vendeurs et acheteurs d’un scénario par ville.
+     *
+     * @param scenario Le nom du fichier scénario
+     * @return Une ArrayList contenant deux maps : [ville -> acheteurs], [ville -> vendeurs]
+     * @throws IDException si une erreur personnalisée est levée
+     * @throws FileNotFoundException si le fichier est introuvable
+     */
     public static ArrayList<Map> regrouperParVille(String scenario) throws IDException, FileNotFoundException {
         List[] acheteurVendeur = lectureScenario(scenario);
-        List achteurL = acheteurVendeur[0];
+        List acheteurL = acheteurVendeur[0];
         List vendeurL = acheteurVendeur[1];
 
-        ArrayList<String> achteur = new ArrayList<>();
+        ArrayList<String> acheteur = new ArrayList<>();
         ArrayList<String> vendeur = new ArrayList<>();
 
-        for (int i = 0; i < achteurL.size(); i++) {
-            achteur.add(achteurL.get(i).toString());
+        for (int i = 0; i < acheteurL.size(); i++) {
+            acheteur.add(acheteurL.get(i).toString());
             vendeur.add(vendeurL.get(i).toString());
         }
-        Map<String, ArrayList<String>> ListeVilleAcheteur;
-        Map<String, ArrayList<String>> ListeVilleVendeur;
-        ListeVilleAcheteur = regroupementParVille(achteur);
-        ListeVilleVendeur = regroupementParVille(vendeur);
+
+        Map<String, ArrayList<String>> ListeVilleAcheteur = regroupementParVille(acheteur);
+        Map<String, ArrayList<String>> ListeVilleVendeur = regroupementParVille(vendeur);
 
         ArrayList<Map> regroupementVille = new ArrayList<>();
         regroupementVille.add(ListeVilleAcheteur);
@@ -119,6 +131,14 @@ public class LectureScenario {
         return regroupementVille;
     }
 
+    /**
+     * Remplace les noms des membres par leur ville dans les listes vendeurs et acheteurs.
+     *
+     * @param scenario Le nom du fichier scénario
+     * @return Un tableau contenant [vendeursVille, acheteursVille]
+     * @throws IDException si une erreur personnalisée est levée
+     * @throws FileNotFoundException si le fichier est introuvable
+     */
     public static List[] remplacementNomVille(String scenario) throws IDException, FileNotFoundException {
         List[] acheteurVendeur = lectureScenario(scenario);
         List<String> vendeurL = acheteurVendeur[0];
@@ -143,6 +163,15 @@ public class LectureScenario {
         return new List[]{vendeursVille, acheteursVille};
     }
 
+    /**
+     * Construit un graphe orienté représentant les livraisons de cartes avec suffixes "+" et "-".
+     * Le graphe contient aussi les sommets Vélizy+ et Vélizy- comme début et fin.
+     *
+     * @param scenario Le nom du fichier scénario
+     * @return Un graphe sous forme Map (ville+ -> [ville-])
+     * @throws IDException si une erreur personnalisée est levée
+     * @throws FileNotFoundException si le fichier est introuvable
+     */
     public static Map<String, List<String>> grapheAvecSuffixes(String scenario) throws IDException, FileNotFoundException {
         List[] donnees = lectureScenario(scenario);
         List<String> vendeurs = donnees[0];
@@ -186,8 +215,14 @@ public class LectureScenario {
         return graphe;
     }
 
-
-
+    /**
+     * Transforme le graphe orienté avec suffixes en un tableau de voisins (graphe sous forme d'adjacence).
+     *
+     * @param scenario Le nom du fichier scénario
+     * @return Tableau d’adjacence : chaque ligne contient les indices des sommets voisins
+     * @throws IDException si une erreur personnalisée est levée
+     * @throws FileNotFoundException si le fichier est introuvable
+     */
     public static int[][] grapheAvecSuffixesEnTabVoisins(String scenario) throws IDException, FileNotFoundException {
         Map<String, List<String>> graphe = grapheAvecSuffixes(scenario);
 
@@ -198,7 +233,7 @@ public class LectureScenario {
         }
 
         List<String> sommetList = new ArrayList<>(sommets);
-        Collections.sort(sommetList); // Optionnel : tri pour cohérence
+        Collections.sort(sommetList); // Tri pour cohérence
         Map<String, Integer> sommetVersIndex = new HashMap<>();
         for (int i = 0; i < sommetList.size(); i++) {
             sommetVersIndex.put(sommetList.get(i), i);
@@ -217,13 +252,6 @@ public class LectureScenario {
             tabVoisins[i] = ligne;
         }
 
-        System.out.println("Index des sommets :");
-        for (int i = 0; i < sommetList.size(); i++) {
-            System.out.println(i + ": " + sommetList.get(i));
-        }
-
         return tabVoisins;
     }
-
-
 }
